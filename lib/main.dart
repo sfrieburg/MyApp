04abd91daebf9fc602999cd30caf370a21bf43de
Sawyer Frieburg <frieb244@d.umn.edu>
2021-11-18 22:18:17 -0600

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'More Cum'),
+      home: const MyHomePage(title: 'A Lot More Cum'),
     );
   }
 }
