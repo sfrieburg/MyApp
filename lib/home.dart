c15059d11f2a8d2bea6e71b8238b5aef37528573
Sawyer Frieburg <frieb244@d.umn.edu>
2021-11-23 23:33:39 -0600

    no pictures:(

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,49 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  PageController pageController = PageController();
+  void onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    pageController.animateToPage(index, duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cum',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Cum Home'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: PageView(
+        controller: pageController,
+        children: [
+          Container(color:Colors.red),
+          Container(color:Colors.green),
+          Container(color:Colors.yellow),
+          Container(color:Colors.blue),
+          Container(color:Colors.purple),
+        ],
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('assets/dude.jpg'),
-          ),
-        ),
-      )
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home')
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        onTap: onTapped
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() => runApp(
-    MaterialApp(
-      initialRoute: '/home',
-      routes: {
-        '/home' : (context) => Home(),
-      },
-    ),
-);
-//test
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const HomeScreen(),
+    );
+  }
+}
