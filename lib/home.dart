8a958e73166f50c4d54db959f849826476998e8c
tmans <tmanshuttle@gmail.com>
2021-12-09 22:52:16 -0600

    new pics

--- lib/home.dart
+++ lib/home.dart
@@ -29,15 +29,17 @@ class _HomeScreenState extends State<HomeScreen> {
           Container(color:Colors.black54),
           Image.asset('assets/dude.jpg'),
           Image.asset('assets/garfield.jpg'),
+          Image.asset('assets/dude.jpg'),
+          Image.asset('assets/kd.jpg'),
           Image.asset('assets/dude.jpg')
         ],
         ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.sports_bar), label: 'Sawyer'),
+          BottomNavigationBarItem(icon: Icon(Icons.sports_bar), label: 'Fuck Head'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Back'),
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home')
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Settings')
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.white,
